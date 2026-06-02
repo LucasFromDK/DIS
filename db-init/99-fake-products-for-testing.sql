d2d41e8ca37c9b1981aaf611d1b444bcfb3cba14
spoody <m1nix@mail.spoodythe.one>
2026-06-02 11:18:23 +0200

    forgor

--- db-init/99-fake-products-for-testing.sql
+++ db-init/99-fake-products-for-testing.sql
@@ -1,4 +1,4 @@
-INSERT INTO sellers (userid) VALUES (1)
+INSERT INTO sellers (userid) VALUES (1);
 INSERT INTO products (sellerid, name, description, price, units) VALUES
 (1, "rusty spoon", "very shartp and rusty", "5995", "8"),
 (1, "Excalibur", "rock sold seperately", "999999999999", "0"),
