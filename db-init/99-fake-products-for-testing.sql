aeb88278001a03518b1a121af2e22bf925a98685
spoody <m1nix@mail.spoodythe.one>
2026-06-02 11:38:30 +0200

    better seller names

--- db-init/99-fake-products-for-testing.sql
+++ db-init/99-fake-products-for-testing.sql
@@ -1,7 +1,6 @@
-INSERT INTO sellers (userid) VALUES (1);
-INSERT INTO sellers (userid) VALUES (2);
+INSERT INTO sellers (userid) VALUES (1),(2),(3),(4);
 INSERT INTO products (sellerid, name, description, price, units) VALUES
 (1, "rusty spoon", "very shartp and rusty", 5995, 8),
-(1, "Excalibur", "rock sold seperately", 999999999999, 0),
-(1, "Faxe Kondi Free", "Sukkerfri sodavand", 4895, 1000),
-(1, "haerte", "orgna", 51925300, 2)
+(2, "Excalibur", "rock sold seperately", 999999999999, 0),
+(3, "Faxe Kondi Free", "Sukkerfri sodavand", 4895, 1000),
+(4, "haerte", "orgna", 51925300, 2)
