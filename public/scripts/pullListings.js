848d4928df92d273c0489d29c92557618cb5519a
Snorre <lkx526@alumni.ku.dk>
2026-06-02 23:14:56 +0200

    throbber :)

--- public/scripts/pullListings.js
+++ public/scripts/pullListings.js
@@ -1,27 +1,31 @@
 // Fetch /api/products and display them on the page
 async function fetchProducts() {
   try {
+    const container = document.querySelector('.listing-container');
+    container.classList.add("loading")
+    container.innerHTML = ''; // Clear existing listings
     const response = await fetch('/api/products');
     if (!response.ok) {
+      container.innerText = await response.text();
+      container.classList.remove("loading")
       throw new Error('Network response was not ok');
     }
     const products = await response.json();
+    container.classList.remove("loading")
     console.log(products)
-    displayProducts(products);
+    displayProducts(container, products);
   } catch (error) {
     console.error('Error fetching products:', error);
   }
 }
 
-function displayProducts(products) {
-  const container = document.querySelector('.listing-container');
-  container.innerHTML = ''; // Clear existing listings
-    products.forEach(product => {
+function displayProducts(container, products) {
+  products.forEach(product => {
     const listing = document.createElement('div');
     listing.classList.add('listing');
 
     let price = new Intl.NumberFormat("da-DK", { style: "currency", currency: "DKK" }).format(
-      product.price/100
+      product.price / 100
     );
 
     let units = new Intl.NumberFormat("da-DK").format(
